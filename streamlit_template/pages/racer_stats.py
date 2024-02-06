59956d57104c2d18dece6ff4c6fdbb8aafefd24b
Justg624 <justg624@gmail.com>
2024-02-05 18:47:39 -0700

    Retry

--- streamlit_template/pages/racer_stats.py
+++ streamlit_template/pages/racer_stats.py
@@ -4,3 +4,9 @@ st.markdown("# Racer Page 🎈")
 st.sidebar.markdown("# Racer Page 🎈")
 
 st.write(' # Mariokart *Stats Website*')
+
+df_racer = pd.read_csv('data/racer_stats.csv')
+
+#st.write(df_racer)
+
+st.dataframe(df_racer.style.highlight_max(color='red', axis=0, subset=['Speed', 'Acceleration']))
